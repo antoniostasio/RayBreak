d1b46fb5454dd05c64bfc7acc510393d488cf8c0
Antonio Stasio <antonio.stasio22@gmail.com>
2017-03-05 20:12:17 +0100

    Rendering moving square

--- RayBreak/Renderer.swift
+++ RayBreak/Renderer.swift
@@ -13,12 +13,27 @@ class Renderer: NSObject {
     let commandQueue: MTLCommandQueue
     
     let vertices: [Float] = [
-        0, 1, 0,
+        -1, 1, 0,
         -1, -1, 0,
-        1, -1, 0
+        1, -1, 0,
+        1, 1, 0
     ]
     
+    let indices : [UInt16] = [
+        0, 1, 2,
+        2, 3, 0
+    ]
+    
+    struct Constants {
+        var animateBy: Float = 0.0
+    }
+    
+    var constants = Constants()
+    
+    var time: Float = 0.0
+    
     var vertexBuffer: MTLBuffer?
+    var indexBuffer: MTLBuffer?
     var pipelineState: MTLRenderPipelineState?
     
     init(device: MTLDevice) {
@@ -29,11 +44,16 @@ class Renderer: NSObject {
         buildPipelineState()
     }
     
+    
     func buildModel() {
         vertexBuffer = device.makeBuffer(bytes: vertices,
                                          length: vertices.count * MemoryLayout<Float>.size,
                                          options: [])
+        indexBuffer = device.makeBuffer(bytes: indices,
+                                        length: indices.count * MemoryLayout<UInt16>.size,
+                                        options: [])
     }
+    
     
     func buildPipelineState() {
         let library = device.newDefaultLibrary()
@@ -53,22 +73,35 @@ class Renderer: NSObject {
 }
 
 extension Renderer: MTKViewDelegate {
+    
     func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
     }
+    
     
     func draw(in view: MTKView) {
         guard let drawable = view.currentDrawable,
             let pipelineState = pipelineState,
+            let indexBuffer = indexBuffer,
             let descriptor = view.currentRenderPassDescriptor else {
                 return
         }
         let commandBuffer = commandQueue.makeCommandBuffer()
         let commandEncoder = commandBuffer.makeRenderCommandEncoder(descriptor: descriptor)
         
+        time += 1 / Float(view.preferredFramesPerSecond)
+        let animatedBy = abs(sin(time)/2 + 0.5)
+        constants.animateBy = animatedBy
+        
         commandEncoder.setRenderPipelineState(pipelineState)
         commandEncoder.setVertexBuffer(vertexBuffer, offset: 0, at: 0)
         
-        commandEncoder.drawPrimitives(type: .triangle, vertexStart: 0, vertexCount: vertices.count)
+        commandEncoder.setVertexBytes(&constants, length: MemoryLayout<Constants>.stride, at: 1)
+        
+        commandEncoder.drawIndexedPrimitives(type: .triangle,
+                                             indexCount: indices.count,
+                                             indexType: .uint16,
+                                             indexBuffer: indexBuffer,
+                                             indexBufferOffset: 0)
         
         commandEncoder.endEncoding()
         commandBuffer.present(drawable)
